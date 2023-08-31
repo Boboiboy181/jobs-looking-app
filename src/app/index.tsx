d8712d5aada309758ab14abe0f6594f4faad4836
Gia Hai <haidao549@gmail.com>
2023-08-31 11:38:09 +0700

    moving files into src folder

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Qua la dep trai tuyet voi ong mat troi</Text>
+    </View>
+  );
+};
+
+export default Home;
